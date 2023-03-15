fa8408c360eda083c18d2d580b3f9c042ab037f1
Abinash Gupta <abgbjb2001@gmail.com>
2023-03-15 23:28:34 +0530

    Second commit

---
+++ Demo/hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
